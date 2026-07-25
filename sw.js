6a080cbf162baf4337cc60f2265f0c4a11d8aae1
ChrisSorbadere <chrissorbadere@gmail.com>
2026-07-25 16:22:09 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE = 'atlas-v2';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  // App shell + same-origin: cache-first
+  if (new URL(req.url).origin === location.origin) {
+    e.respondWith(
+      caches.match(req).then(hit => hit || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+        return res;
+      }).catch(() => caches.match('./index.html')))
+    );
+    return;
+  }
+  // Cross-origin (fonts): stale-while-revalidate
+  e.respondWith(
+    caches.match(req).then(hit => {
+      const net = fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+        return res;
+      }).catch(() => hit);
+      return hit || net;
+    })
+  );
+});
